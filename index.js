1456dd82812b790f0458e9cbbe00736c55e2ebe9
othon <othon_ferreira@ia.com>
2018-08-08 17:13:16 -0300

    Changed code logic

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ exports.interceptor = (instance,methodName,{beforeFn=[],afterFn=[]}=functions)=>
   const isArrayObject = obj => Object.prototype.toString.call(obj) === '[object Array]'
   const makeArray = fn => isArrayObject(fn) ? fn : [fn]
 
-  const oldMethod = instance[methodName]
+  const originalMethod = instance[methodName]
   beforeFn = makeArray(beforeFn)
   afterFn = makeArray(afterFn)
 
   instance[methodName] = ()=>{
     beforeFn.forEach( fn=>fn.apply(instance) )
-    oldMethod.apply(instance, arguments)
-    afterFn.forEach(fn=>fn.apply(instance))
+    originalMethod.apply(instance, arguments)
+    afterFn.forEach(fn=>fn.call(instance))
   }
 
   return instance
