d7b6ac90c24eee970db3f9426b2695c83e277da3
othon <othon_ferreira@ia.com>
2018-08-13 17:06:51 -0300

    Added regex feature

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const {promisify} = require('util')
 
-exports.interceptor = async (instance,methodName,{beforeFn=[],afterFn=[]}=functions)=>{
+exports.interceptor = (instance,methodsRegex,{beforeFn=[],afterFn=[]}=functions)=>{
   const isArrayObject = obj => Object.prototype.toString.call(obj) === '[object Array]'
+  const isFunction = fn => Object.prototype.toString.call(fn) === '[object Function]'
+  const makeRegexString = (accumulator, currentValue) => `$(accumulator)|$(currentValue)`
   const makeArray = fn => isArrayObject(fn) ? fn : [fn]
 
-  const originalMethodPromise = promisify(instance[methodName])
+  methodsRegex = makeArray(methodsRegex)
+  matchMethod = Object.keys(instance)
+    .filter(i => isFunction(i))
+    .filter(i => methodsRegex.includes(i))
   beforeFn = makeArray(beforeFn)
   afterFn = makeArray(afterFn)
 
-  beforeFn.forEach(fn=>fn.apply(instance))
-  const resultOriginalFunc = await originalMethodPromise()
-  afterFn.forEach(fn=>fn.call(instance))
+  matchMethod.forEach(async method =>{
+    const originalMethodPromise = promisify(instance[method])
 
-  return resultOriginalFunc
+    beforeFn.forEach(fn=>fn.apply(instance))
+    const resultOriginalFunc = await originalMethodPromise()
+    afterFn.forEach(fn=>fn.call(instance))
+
+  })
+
+
 }
